ae40310b5b874f1b4ff3dfc51b7e309796286e09
root <root@jakes-mac.localdomain>
2022-08-07 08:30:31 -0400

    complete part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+class Finch:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, color, sound, age):
+    self.name = name
+    self.color = color
+    self.sound = sound
+    self.age = age
+
+finches = [
+  Finch('Lolo', 'tabby', 'Kinda rude.', 3),
+  Finch('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Finch('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Finch('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', { 'finches': finches })
